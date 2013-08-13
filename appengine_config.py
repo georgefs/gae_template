31c0bb7b7a6225054ea46d1c544aaa8e83e8c6df
georgefs <goblin.george@gmail.com>
2013-08-13 12:13:28 +0800

    clear debug information

--- appengine_config.py
+++ appengine_config.py
@@ -3,8 +3,5 @@ import os
 
 CUR_PATH = os.path.dirname(os.path.abspath(__file__))
 SITE_PATH = os.path.join(CUR_PATH, 'site-packages/')
-APPS_PATH = os.path.join(CUR_PATH, 'apps/')
-print SITE_PATH
 
 site.addsitedir(SITE_PATH)
-site.addsitedir(APPS_PATH)
